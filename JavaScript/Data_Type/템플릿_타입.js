7b315da86cafe14784b8ab1f212e65911ba88532
BuildEnough <rhtmaehcl878@naver.com>
2023-03-24 11:48:55 +0900

    JavaScript: 데이터 타입

---
+++ JavaScript/Data_Type/템플릿_타입.js
@@ -0,0 +1,14 @@
+var template = `Template literal`;
+console.log(template); // Template literal
+
+
+var template = '<ul>\n\t<li><a href="#">Home</a></li>\n</ul>';
+console.log(template);
+
+
+var template = `<ul>
+  <li><a href="#">Home</a></li>
+</ul>`;
+console.log(template);
+
+
