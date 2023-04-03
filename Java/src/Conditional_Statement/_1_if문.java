b742641a8844641b3d20959d898c82e892999287
BuildEnough <rhtmaehcl878@naver.com>
2023-04-03 23:13:09 +0900

    Java: 조건문

---
+++ Java/src/Conditional_Statement/_1_if문.java
@@ -0,0 +1,15 @@
+package Conditional_Statement;
+
+public class _1_if문 {
+    public static void main(String[] args) {
+        int x = 0;
+
+        if (x == 0) System.out.println(0);
+
+        x = 1;
+
+        if (x != 0) {
+            System.out.println(1);
+        }
+    }
+}
