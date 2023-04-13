217f96d960c0d82b45d64d18b4a79854b6dacd8f
BuildEnough <rhtmaehcl878@naver.com>
2023-04-13 23:53:23 +0900

    Java: 참조형 매개변수

---
+++ Java/src/ObjectOrientedProgramming/_10_참조형_매개변수1.java
@@ -0,0 +1,19 @@
+package ObjectOrientedProgramming;
+
+class Data2 { int x;}
+public class _10_참조형_매개변수1 {
+    public static void main(String[] args) {
+        Data2 d = new Data2();
+        d.x = 10;
+        System.out.println(d.x);
+
+        change(d);
+        System.out.println(d.x);
+    }
+
+    static void change(Data2 d) {
+        d.x = 1000;
+        System.out.println("change");
+        System.out.println(d.x);
+    }
+}
