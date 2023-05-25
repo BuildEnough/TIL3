cc0b2bdc5b4dda82eb55754cba1838e46dd959ee
BuildEnough <rhtmaehcl878@naver.com>
2023-05-25 15:19:15 +0900

    return 문 조건

--- Java/src/MyMathTest.java
+++ Java/src/MyMathTest.java
@@ -1,21 +1,18 @@
 public class MyMathTest {
     public static void main(String[] args) {
         MyMath mm = new MyMath();
-        mm.printGugudan(6);
+        int result = mm.max(5, 3);
+
+        System.out.println(result);
     }
 
 }
 
 class MyMath {
-    void printGugudan(int dan) {
-        if (!(2<=dan && dan <=9)){
-            return; // 입력받은 값이 2~9가 아니면, 메서드 종료 후 돌아가기
-        }
-
-
-        for (int i = 1; i <= 9; i++) {
-            System.out.printf("%d * %d = %d%n", dan, i, dan * i);
-        }
-        // return; // return문 생략 가능
+    int max(int a, int b) {
+        if(a > b)
+            return a;
+        else
+            return b;
     }
 }
