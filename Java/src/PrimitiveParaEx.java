ca787f3725e07778f5bc000b5051f8df373b79ca
BuildEnough <rhtmaehcl878@naver.com>
2023-05-26 09:37:14 +0900

    기본형 매개변수

--- Java/src/PrimitiveParaEx.java
+++ Java/src/PrimitiveParaEx.java
@@ -6,12 +6,12 @@ public class PrimitiveParaEx {
         d.x = 10;
         System.out.println(d.x); // 10
 
-        change(d);
-        System.out.println(d.x);// 1000
+        change(d.x);
+        System.out.println(d.x);// 10
 
     }
-    static void change(Data d) { // 참조형 매개변수
-        d.x = 1000;
-        System.out.println("chage: " + d.x); // chage: 1000
+    static void change(int d) { // 기본형 매개변수
+        d = 1000;
+        System.out.println("chage: " + d); // chage: 1000
     }
 }
