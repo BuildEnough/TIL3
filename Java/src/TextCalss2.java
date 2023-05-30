4b4a2fbbd217bfe5359e851ef23f9a3da1ebec6d
BuildEnough <rhtmaehcl878@naver.com>
2023-05-30 13:27:54 +0900

    인스턴스 메서드와 클래스 메서드

---
+++ Java/src/TextCalss2.java
@@ -0,0 +1,14 @@
+public class TextCalss2 {
+    int iv; // 인스턴스 변수
+    static int cv; // 클래스 변수
+
+    void instanceMethod() { // 인스턴스 메서드
+        System.out.println(iv); // 인스턴스 변수 사용 가능
+        System.out.println(cv); // 클래스 변수 사용 가능
+    }
+
+    static void staticMethod() {
+        // System.out.println(iv); // 에러, 인스턴스 변수 사용 불가능
+        System.out.println(cv); // 클래스 변수 사용 가능
+    }
+}
