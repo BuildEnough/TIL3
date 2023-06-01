c6e102aba47b43bf8b4d24000b7b24e6f6585a9a
BuildEnough <rhtmaehcl878@naver.com>
2023-06-01 19:33:26 +0900

    this

--- Java/src/CarTest.java
+++ Java/src/CarTest.java
@@ -3,22 +3,26 @@ class Car {
     String gearType;
     int door;
 
-    Car() {} // 생성자
-    Car(String c, String g, int d) { // 매개변수가 있는 생성자
-        color = c;
-        gearType = g;
-        door = d;
+    Car() {
+        this("white", "auto", 4);
+    }
+
+    Car(String color) {
+        this(color, "auto", 4);
+    }
+
+    Car(String color, String gearType, int door) {
+        this.color = color;
+        this.gearType = gearType;
+        this.door = door;
     }
 }
 
 class CarTest {
     public static void main(String[] args) {
         Car c1 = new Car();
-        c1.color = "white"; // 인스턴스 변수 따로 초기화
-        c1.gearType = "auto"; // 인스턴스 변수 따로 초기화
-        c1.door = 4; // 인스턴스 변수 따로 초기화
+        Car c2 = new Car("blue", "autoo", 5);
 
-        Car c2 = new Car("white", "auto", 5); // 인스턴스를 생성하는 동시에 원하는 값으로 초기화
         System.out.println(c1.color +" "+  c1.gearType +" "+ c1.door);
         System.out.println(c2.color +" "+  c2.gearType +" "+ c2.door);
     }
