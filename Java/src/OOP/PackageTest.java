e472636d917655a72e683c395dd72ec4d98277cc
BuildEnough <rhtmaehcl878@naver.com>
2023-06-08 14:28:26 +0900

    import 문

--- Java/src/OOP/PackageTest.java
+++ Java/src/OOP/PackageTest.java
@@ -1,11 +1,13 @@
 package OOP;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+//import java.util.*;
+
 public class PackageTest {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Date today = new Date();
+//        java.util.Date today = new java.util.Date();
+        SimpleDateFormat date = new SimpleDateFormat("yyyy/mm/dd");
     }
-}
-
-class PackageTest2 {
-
 }
