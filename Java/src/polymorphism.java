6bbce7d1e66b61efd46de8a0c579eeb2d29d2420
BuildEnough <rhtmaehcl878@naver.com>
2023-06-13 17:03:44 +0900

    하나의 배열로 여러 종류 객체 다루기

--- Java/src/polymorphism.java
+++ Java/src/polymorphism.java
@@ -6,12 +6,12 @@ class Product {
         this.price = price;
         bonusPoint = (int) (price / 10.0);
     }
+
+    Product() {} // 기본 생성자
 }
 
 class Tv1 extends Product {
-    Tv1() {
-        super(100); // 조상클래스의 생성자 Product(int price) 호출
-    }
+    Tv1() { super(100); }
 
     public String toString() { return "Tv"; }
 }
@@ -23,9 +23,18 @@ class Computer extends Product {
     public String toString() { return "Computer"; }
 }
 
+class Audio extends Product {
+    Audio() {
+        super(50);
+    }
+    public String toString() { return "Audio"; }
+}
+
 class Buyer {
     int money = 1000;
     int bonusPoint = 0;
+    Product[] cart = new Product[10];
+    int i = 0;
 
     void buy(Product p) {
         if (money < p.price) {
@@ -35,10 +44,26 @@ class Buyer {
 
         money -= p.price;
         bonusPoint += p.bonusPoint;
+        cart[i++] = p;
         System.out.println(p + "를 구입했습니다");
+    }
 
+    void summary() {
+        int sum = 0;
+        String itemList = "";
+
+        for (int i = 0; i < cart.length; i++) {
+            if (cart[i] == null) break;
+            sum += cart[i].price;
+            itemList += cart[i] + ", ";
+            // itemList += cart[i].toString() + ", ";
+        }
+        System.out.println("sum: " + sum);
+        System.out.println("itemList: " + itemList);
     }
 }
+
+
 
 public class polymorphism {
     public static void main(String[] args) {
@@ -46,8 +71,7 @@ public class polymorphism {
 
         b.buy(new Tv1());
         b.buy(new Computer());
-
-        System.out.println(b.money);
-        System.out.println(b.bonusPoint);
+        b.buy(new Audio());
+        b.summary();
     }
 }
