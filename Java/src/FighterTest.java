5c00dbb87b37de8ca49c6313dd5891bd1a392ff2
BuildEnough <rhtmaehcl878@naver.com>
2023-06-15 16:03:30 +0900

    인터페이스를 이용한 다형성

---
+++ Java/src/FighterTest.java
@@ -0,0 +1,35 @@
+abstract class Unit {
+    int x, y;
+
+    abstract void move(int x, int y);
+
+    void stop() { System.out.println("멈춤"); }
+}
+
+interface Fightable {
+    void move(int x, int y); // public abstract 생략
+    void attack(Fightable f); // public abstract 생략
+}
+
+class Fighter extends Unit implements Fightable {
+    // public 붙이는 이유: 오버라이딩 규칙 중 조상보다 접근제어자가 좁으면 안됨
+    public void move(int x, int y) {
+        System.out.printf("x: %d, y: %d%n", x, y);
+    }
+    public void attack(Fightable f) {
+        System.out.println(f + "공격");
+    }
+
+    Fightable getFightable() {
+        Fightable f = new Fighter();
+        return f;
+    }
+}
+
+public class FighterTest {
+    public static void main(String[] args) {
+        Fighter f = new Fighter();
+        Fightable f2 = f.getFightable(); // Fightable 반환타입
+
+    }
+}
