864774f25943fd991b85cb26bdcfa5453f22fb64
BuildEnough <rhtmaehcl878@naver.com>
2023-06-22 22:47:45 +0900

    unchecked Exception

--- Java/src/ExceptionTest.java
+++ Java/src/ExceptionTest.java
@@ -1,5 +1,5 @@
 class ExceptionTest {
     public static void main(String args[]) {
-        throw new Exception(); // Exception을 고의로 발생시킨다
+        throw new RuntimeException(); // RuntimeException을 고의로 발생시킨다
     }
 }
