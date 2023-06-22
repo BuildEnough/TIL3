4b724925399e53334e6680435272934854a20eba
BuildEnough <rhtmaehcl878@naver.com>
2023-06-22 22:28:09 +0900

    checked Exception

--- Java/src/ExceptionTest.java
+++ Java/src/ExceptionTest.java
@@ -1,18 +1,5 @@
-public class ExceptionTest {
-    public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        try {
-            System.out.println(3);
-            System.out.println(args[0]); // ArrayIndexOutOfBoundException
-            System.out.println(4); // 실행되지 않는다
-        } catch (ArithmeticException ae) {
-            if (ae instanceof ArithmeticException)
-                System.out.println("true");
-            System.out.println("ArithmeticException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("ArrayIndexOutOfBoundsException");
-        }  // try-catch의 끝
-        System.out.println(6);
+class ExceptionTest {
+    public static void main(String args[]) {
+        throw new Exception(); // Exception을 고의로 발생시킨다
     }
 }
