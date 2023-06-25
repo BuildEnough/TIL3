c8fe082b0f246867ec7869b4bb2f222cdd6bddd4
BuildEnough <rhtmaehcl878@naver.com>
2023-06-25 16:37:55 +0900

    toString의 오버라이딩

--- Java/src/Card.java
+++ Java/src/Card.java
@@ -1,51 +1,47 @@
+import java.util.Objects;
+
 class Card {
     String kind ;  // 카드의 무늬 - 인스턴스 변수
     int number;  // 카드의 숫자 - 인스턴스 변수
-    static int width = 100;  // 카드의 폭  - 클래스 변수
-    static int height = 250;  // 카드의 높이 - 클래스 변수
+
+    Card() {
+        this("SPADE", 1);
+    }
+
+    Card(String kind, int number) {
+        this.kind = kind;
+        this.number = number;
+    }
+
+    // equals()를 오버라이딩하면 hashCode()도 오버라이딩 해야한다
+    public int hashCode() {
+        return Objects.hash(kind, number);
+    }
+
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Card)) {
+            return false;
+        }
+        Card c = (Card)obj;
+        return this.kind.equals(c.kind) && this.number == c.number;
+
+    }
+
+    // Object 클래스의 toString()을 오버라이딩
+    public String toString() {
+        return "kind: " + kind + ", number: " + number;
+    }
 }
+
 
 class CardTest{
     public static void main(String args[]) {
-        System.out.println(Card.width); // 100
-        System.out.println(Card.height); // 250
-        System.out.println();
-
         Card c1 = new Card();
-        c1.kind = "Heart";
-        c1.number = 7;
-
         Card c2 = new Card();
-        c2.kind = "Spade";
-        c2.number = 4;
 
-        System.out.println(c1.kind); // Heart
-        System.out.println(c1.number); // 7
-        System.out.println(c1.width); // 100
-        System.out.println(c1.height); // 250
-        System.out.println();
+        System.out.println(c1.equals(c2));
 
-        System.out.println(c2.kind); // Spade
-        System.out.println(c2.number); // 4
-        System.out.println(c2.width); // 100
-        System.out.println(c2.height); // 250
-        System.out.println();
-
-
-        // Card의 width = 50, height = 80 변경
-        Card.width = 50;
-        Card.height = 80;
-
-        System.out.println(c1.kind); // Heart
-        System.out.println(c1.number); // 7
-        System.out.println(c1.width); // 50
-        System.out.println(c1.height); // 80
-        System.out.println();
-
-        // c1과 c2는 클래스 변수를 공유하기 때문에 같이 바뀐다
-        System.out.println(c2.kind); // Spade
-        System.out.println(c2.number); // 4
-        System.out.println(c2.width); // 50
-        System.out.println(c2.height); // 80
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
     }
 }
