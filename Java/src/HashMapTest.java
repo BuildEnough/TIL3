d8df4696a73608cd7a16f614d7884afa220311e7
BuildEnough <rhtmaehcl878@naver.com>
2023-07-27 08:46:58 +0900

    HashMap과 지네릭스

--- Java/src/HashMapTest.java
+++ Java/src/HashMapTest.java
@@ -1,7 +1,16 @@
+import java.awt.dnd.DragGestureEvent;
 import java.util.*;
 
 class HashMapTest {
     public static void main(String[] args) {
+        // HashMap<String, Student> map = new HashMap<String, Student>();
+        HashMap<String, Student> map = new HashMap(); // <String, Student> 생략 가능
+        map.put("Apple", new Student("Apple", 1, 1, 100, 100, 100));
+
+        Student s = map.get("Apple");
+
+        System.out.println(map);
+        System.out.println(map.get("Apple").name);
     }
 }
 
@@ -9,10 +18,15 @@ class Student {
     String name = "";
     int ban;
     int no;
+    int kor;
+    int eng;
+    int math;
 
-    Student(String name, int ban, int no) {
+    Student(String name, int ban, int no, int kor, int eng, int math) {
         this.name = name;
         this.ban = ban;
-        this.no = no;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
     }
 }
