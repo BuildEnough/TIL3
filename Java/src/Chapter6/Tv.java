c9373ec24dd8e00e5830b080896cd9714fa19dc9
BuildEnough <rhtmaehcl878@naver.com>
2023-09-01 13:21:58 +0900

    자바 복습

---
+++ Java/src/Chapter6/Tv.java
@@ -0,0 +1,60 @@
+package Chapter6;
+
+class Tv {
+    String color;
+    boolean power;
+    int channel;
+
+    void power() {
+        power = !power;
+    }
+
+    void channelUp() {
+        ++channel;
+    }
+
+    void channelDown() {
+        --channel;
+    }
+}
+
+class TvTest {
+    public static void main(String[] args) {
+        Tv t = new Tv();
+        t.channel = 5;
+        System.out.println(t.channel);
+
+        t.channelDown();
+        System.out.println(t.channel);
+
+        t.channelUp();
+        System.out.println(t.channel);
+    }
+}
+
+class TvTest2 {
+    public static void main(String[] args) {
+        Tv t2 = new Tv();
+        t2.channel= 10;
+        System.out.println(t2.channel);
+
+        t2.channelDown();
+        t2.channelDown();
+        System.out.println(t2.channel);
+    }
+}
+
+
+class TvTest3 {
+    public static void main(String[] args) {
+        Tv t1 = new Tv();
+        Tv t2 = new Tv();
+        System.out.println(t1.channel);
+        System.out.println(t2.channel);
+
+        t2 = t1;
+        t1.channel = 7;
+        System.out.println(t1.channel);
+        System.out.println(t2.channel);
+    }
+}
