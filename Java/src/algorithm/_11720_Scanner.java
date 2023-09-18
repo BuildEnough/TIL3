f841197235d958a609bce5e7bcb4de84169129ea
BuildEnough <rhtmaehcl878@naver.com>
2023-09-18 15:12:30 +0900

    자바 연습

---
+++ Java/src/algorithm/_11720_Scanner.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class _11720_Scanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < cNum.length; i++) {
+            sum += cNum[i] - '0';
+        }
+        System.out.println(sum);
+    }
+}
---
+++ Java/src/algorithm/_1546_Scanner.java
@@ -0,0 +1,27 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class _1546_Scanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+
+        for (int i = 0; i < A.length; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        long sum = 0;
+        long max = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > max) {
+                max = A[i];
+            }
+            sum += A[i];
+        }
+
+        System.out.println(sum * 100 / max / (float)A.length);
+    }
+}
